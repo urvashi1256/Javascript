1c992aadc9d60f676cc2557a2256dc9a7dcf6356
Urvashi Salonia <119739348+urvashi1256@users.noreply.github.com>
2024-01-06 06:34:48 +0000

    configuration config done

---
+++ Basics/variable.js
@@ -0,0 +1,11 @@
+const accountID =12455
+let accountEmail = "urvashisalonia@gmail.com"
+var accountPasword="29305"
+accountPasword="29335"
+// accountID =2 not allowed
+accountCity="Kaithal"
+accountCity="Siwan"
+
+accountEmail = "vashisalonia@gmail.com"
+
+console.log([accountEmail,accountID,accountPasword,accountCity]);
